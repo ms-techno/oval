6933a32088a912e40de3dad4f2f4b5914cc204ae
Eslam <eslam.rezk96@hotmail.com>
2025-09-23 21:54:35 +0300

    [ADD]add-ons

---
+++ custom/ms_purchase_post_bill/__manifest__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "MS Purchase post bill",
+    'summary': "when create bill auto post, make complete override for action_create_invoice method",
+    'author': "MS Technologies",
+    'website': "https://www.mstech-eg.com/",
+    'depends': ['account', 'purchase'],
+    'data': [
+    ]
+}
