31f922c48b01a017ebd35da4a41f04b59709f994
Eslam <eslam.rezk96@hotmail.com>
2025-10-02 22:51:13 +0300

    [update]move invoice journal field place in sale order form

---
+++ custom/ms_sale_custom/__manifest__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "MS Sale Custom",
+    'summary' : "Customizations for Sales Module",
+    'author': "MS Technologies",
+    'website': "https://www.mstech-eg.com/",
+    'depends': ['sale'],
+    'data': [
+        'views/sale_order.xml',
+    ]
+}
